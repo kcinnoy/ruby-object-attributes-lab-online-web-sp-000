06e62662f7771eeb49dd846d8876ffd54c9a87f2
Yonnick Brown <25hour24@gmail.com>
2019-01-22 19:59:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,3 @@
-#
-# class Dog
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-#   def name=(new_name)
-#     @name= new_name
-#   end
-#
-# end
-
-# fido = Dog.new("Fido")
-#
-# fido.name = "Fido"
 
 class Dog
   def name=(dogs_name)
